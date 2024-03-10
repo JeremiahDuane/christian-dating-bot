9b4e74f7ad4cfb252f52ab5847abbfd9c6e5fd7d
Jeremiah Gage <jgage6@oreillyauto.com>
2024-03-10 14:47:31 -0500

    Add paths lib

--- src/bot.ts
+++ src/bot.ts
@@ -1,36 +1,47 @@
-import { GatewayIntentBits } from 'discord.js';
-import { DiscordClient } from '@/classes/Discord';
-import { commands } from '@/commands';
-import { Bot } from '@/classes/Bot';
-import { RedditClient } from '@/classes/Reddit';
-import { SnoowrapOptions } from 'snoowrap';
+import { GatewayIntentBits } from "discord.js";
+import { DiscordClient } from "@/classes/Discord";
+import { commands } from "@/commands";
+import { Bot } from "@/classes/Bot";
+import { RedditClient } from "@/classes/Reddit";
+import { SnoowrapOptions } from "snoowrap";
 import { configDotenv } from "dotenv";
 
-configDotenv()
+configDotenv();
 
-const { DISCORD_CLIENT_ID, DISCORD_GUILD_ID, DISCORD_TOKEN, REDDIT_USER_AGENT, REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USERNAME, REDDIT_PASSWORD } = process.env;
+const {
+  DISCORD_CLIENT_ID,
+  DISCORD_GUILD_ID,
+  DISCORD_TOKEN,
+  REDDIT_USER_AGENT,
+  REDDIT_CLIENT_ID,
+  REDDIT_CLIENT_SECRET,
+  REDDIT_USERNAME,
+  REDDIT_PASSWORD,
+} = process.env;
 
 const discordLoginOptions = {
-    clientId: DISCORD_CLIENT_ID,
-    guildId: DISCORD_GUILD_ID,
-    token: DISCORD_TOKEN,    
-}
+  clientId: DISCORD_CLIENT_ID,
+  guildId: DISCORD_GUILD_ID,
+  token: DISCORD_TOKEN,
+};
 
 const redditLoginOptions: SnoowrapOptions = {
-    userAgent: REDDIT_USER_AGENT,
-    clientId: REDDIT_CLIENT_ID,
-    clientSecret: REDDIT_CLIENT_SECRET,
-    username: REDDIT_USERNAME,
-    password: REDDIT_PASSWORD
-}
+  userAgent: REDDIT_USER_AGENT,
+  clientId: REDDIT_CLIENT_ID,
+  clientSecret: REDDIT_CLIENT_SECRET,
+  username: REDDIT_USERNAME,
+  password: REDDIT_PASSWORD,
+};
 
-const discordOptions = { 
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] 
-}
+const discordOptions = {
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+};
 
-export default new Bot(
-    {
-        discord: new DiscordClient(discordOptions, discordLoginOptions, commands),
-        reddit: new RedditClient(redditLoginOptions)
-    }
-)
+export default new Bot({
+  discord: new DiscordClient(discordOptions, discordLoginOptions, commands),
+  reddit: new RedditClient(redditLoginOptions),
+});
--- src/discord/deployCommands.ts
+++ src/discord/deployCommands.ts
@@ -1,27 +1,31 @@
-import { REST } from 'discord.js';
-import dotenv from 'dotenv';
-import { log } from '@/log';
-import { commands } from '@/commands';
+import { REST } from "discord.js";
+import dotenv from "dotenv";
+import { log } from "@/log";
+import { commands } from "@/commands";
 dotenv.config();
 
-const { DISCORD_CLIENT_ID: clientId, DISCORD_GUILD_ID: guildId, DISCORD_TOKEN: token } = process.env;
+const {
+  DISCORD_CLIENT_ID: clientId,
+  DISCORD_GUILD_ID: guildId,
+  DISCORD_TOKEN: token,
+} = process.env;
 
 export async function deployCommands() {
-    if (!token) return;
-    const rest = new REST({ version: '9' }).setToken(token);
+  if (!token) return;
+  const rest = new REST({ version: "9" }).setToken(token);
 
-    try {
-        log(`Started refreshing ${[...commands].length} application (/) commands.`);
+  try {
+    log(`Started refreshing ${[...commands].length} application (/) commands.`);
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            `/applications/${clientId}/guilds/${guildId}/commands`,
-            { body: commands },
-        ) as Array<String>;
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = (await rest.put(
+      `/applications/${clientId}/guilds/${guildId}/commands`,
+      { body: commands }
+    )) as Array<String>;
 
-        log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        // And of course, make sure you catch and log any errors!
-        log("Failed in deploy-commands.ts", error);
-    }
+    log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    log("Failed in deploy-commands.ts", error);
+  }
 }
